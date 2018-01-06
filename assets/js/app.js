9d0ba3f35b4b358136a8f5100c7fa62097c04334
Lullabylullaby <34788419+Lullabylullaby@users.noreply.github.com>
2018-01-06 18:18:32 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,7 @@
 // 始初化辅助log
 var consoler = new VConsole();
 
-var body = document.getElementsByTagName("body")[0];
+var tool = document.getElementById("tool");
 setInterval(function(){
-  var colorStr = body.style.backgroundColor.substr(1);
-  console.log( colorStr , parseInt(colorStr,16) , parseInt(colorStr,16).toString(16) );
-  body.style.backgroundColor = "#" + Math.ceil( parseInt(colorStr,16) - 20 ).toString(16) ;
-},300)
+  tool.innerHtml = "" + new Date();
+},1000)
